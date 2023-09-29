8bc2233ac7727efa0e9bf78ed83ffa410aa4297a
jnbila <jihannabilanazhif@gmail.com>
2023-09-29 14:21:30 +0700

    tugas 1 wp2

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Jihan Nabila Nazhif
+        Saya tinggal di daerah Jakarta Utara
+        Olah raga yang saya sukai adalah
+        Basket";
+    }
+}
